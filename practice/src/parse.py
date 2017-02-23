82997f10e9b3ad1cf698f8f922e800ce3301047a
quody <Aleksi@Aleksis-MacBook-Pro.local>
2017-02-23 17:25:00 +0200

    Fix t/m validation and syntax

--- practice/src/parse.py
+++ practice/src/parse.py
@@ -7,7 +7,7 @@ class Pizza(object):
         metadata = f.readline().split(' ')
         self.rows = int(metadata[0])
         self.columns = int(metadata[1])
-        self.minIngredients = metadata[2]
+        self.minIngredients = int(metadata[2])
         self.maxCells = metadata[3]
         self.pizza = np.empty([self.rows, self.columns])
         self.pizza.astype(int)
@@ -33,6 +33,12 @@ class Pizza(object):
                 sys.stdout.write(self.enumToChar(column))
             sys.stdout.write('\n')
 
+    def printSlices(self):
+        for row in self.slices:
+            for column in row:
+                sys.stdout.write(str(column))
+            sys.stdout.write('\n')
+
     def charToEnum(self, c):
         return {
             'T': 0,
@@ -45,8 +51,7 @@ class Pizza(object):
             1: 'M',
         }[e]
 
-    def addSlice(r1, r2, c1, c2):
-        error = False
+    def addSlice(self, r1, r2, c1, c2):
         tomatoCount = 0
         mushroomCount = 0
 
@@ -59,34 +64,32 @@ class Pizza(object):
 
         #Check size validity
         size = (r2-r1+1)*(c2-c1+1)
-        if (size > maxCells):
+        if (size > self.maxCells):
             return False
 
         #Check that the area is empty and has both stuffings
-        for row in range(r1, r2):
-            for column in range(c1, c2):
+        for row in range(r1, r2 + 1):
+            for column in range(c1, c2 + 1):
                 if (self.slices[row][column] != 0):
-                    error = True
+                    return True #Slice conflict
                 if (self.pizza[row][column] == 0):
                     tomatoCount += 1
                 if (self.pizza[row][column] == 1):
                     mushroomCount += 1
-        if (error):
-            return False
         if (tomatoCount < self.minIngredients or mushroomCount < self.minIngredients):
             return False
 
         #If everything is ok add the slice
-        for row in range(r1, r2):
-            for column in range(c1, c2):
+        for row in range(r1, r2 + 1):
+            for column in range(c1, c2 + 1):
                 self.slices[row][column] = self.sliceCounter
         self.sliceCounter += 1
         return True
 
     def result():
         pizzaUsage = 0
-        for row in range(r1, r2):
-            for column in range(c1, c2):
+        for row in range(r1, r2 + 1):
+            for column in range(c1, c2 + 1):
                 if (self.slices[row][column] != 0):
                     pizzaUsage += 1
         return pizzaUsage
@@ -94,3 +97,5 @@ class Pizza(object):
 
 pitsu = Pizza('../data/small.in')
 print pitsu.printPizza()
+pitsu.addSlice(0, 1, 0, 1)
+print pitsu.printSlices()
