856f6125b2628f76bada732cb0dac4fdfa03bd5e
Laged <parkkila.matti@gmail.com>
2017-02-23 22:22:33 +0200

    Initial class parsing

--- competition/src/parse.py
+++ competition/src/parse.py
@@ -5,6 +5,7 @@ import operator
 
 class Cache(object):
     def __init__(self, id, size):
+        print 'Cache ' + str(id) + ' ' + str(size)
         self.id = id
         self.size = size
         self.candidates = []
@@ -48,32 +49,38 @@ class Cache(object):
 
 
 
-
-class CacheList(object):
-    def __init__(self, cacheCount, cacheSize):
-        self.list = [] # TODO initialize this with Cache objects with real ids
+#List of Cache objects
+cacheList = []
 
 class Video(object):
     def __init__(self, id, size):
+        print 'Video ' + str(id) + ' ' + str(size)
         self.id = id
         self.size = size
 
-class VideoList(object):
-    def __init__(self, videoSizes):
-        self.list = [] # TODO List of the Video objects
+#class VideoList(object):
+#    def __init__(self, videoSizes):
+#        self.list = [] # TODO List of the Video objects
+
+#List of Video objects
+videoList = []
 
 class Endpoint(object):
-    def __init__(self, latencyList, requestList):
+    def __init__(self, id, dcLatency):
+        print 'Endpoint ' + str(id) + ' ' + str(dcLatency)
         self.latencies = {}
+        self.id = id
         self.requests = {} # {Video.id, requestCount}
         self.caches = self.latencies.keys()
-        self.dc_latency = 0
+        self.dc_latency = dcLatency
+    def setLatencyToCache(self, cacheId, latency):
+        self.latencies[cacheId] = latency
 
-class EndpointList(object):
-    def __init__(self, latencyList):
-        self.list = [] # TODO List of Endpoint objects
+#List of Endpoint objects
+endpointList = []
 
 def loadData(path):
+    # Parse and print metadata
     print 'Parsing: ' + path
     file = open(path)
     metadata = file.readline().split(' ')
@@ -88,7 +95,47 @@ def loadData(path):
     print 'cacheCount ' + str(cacheCount)
     print 'cacheSize ' + str(cacheSize)
 
+    # Initialize actual classes
+    # Initalize caches 
+    for cacheId in range(cacheCount):
+        cacheList.append(Cache(len(cacheList), cacheSize))
+
+    # Initialize VideoList
+    videoSizes = map(int, file.readline().split(' '))
+    # TODO INITIALIZE VIDEOS
+    for videoId in range(len(videoSizes)):
+        videoList.append(Video(videoId, videoSizes[videoId]))
+    print 'len videoList ' + str(len(videoList))
+    # Initialize endpoints
+    states = ["cacheDescription", "cacheData", "request"]
+    state = "cacheDescription"
+    dataLeft = 0
+    for line in file:
+        lineData = line.split(' ')
+        if len(lineData) == 3:
+            state = "request"
+        if state == "cacheDescription":
+            dcDelay = int(lineData[0])
+            endpointList.append(Endpoint(len(endpointList), dcDelay))
+            dataLeft = int(lineData[1])
+            state = "cacheData"
+        elif state == "cacheData":
+            #TODO
+            cacheId = int(lineData[0])
+            latency = int(lineData[1])
+            endpointList[len(endpointList) - 1].setLatencyToCache(cacheId, latency)
+            dataLeft -= 1
+            if dataLeft == 0:
+                state = "cacheDescription"
+        elif state == "request":
+            #TODO
+            print "requestData"
+            print lineData
+
 if __name__ == "__main__":
     dataPath = '../data/' + sys.argv[1] + '.in'
     print 'dataPath ' + dataPath
     loadData(dataPath)
+    print cacheList
+    print endpointList
+    print videoList
